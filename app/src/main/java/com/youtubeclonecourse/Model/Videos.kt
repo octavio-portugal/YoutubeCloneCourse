2af445ea8b78969253d4bd9d041ac13f3c52d04c
Octavio Portugal <octaviolportugal@gmail.com>
2021-02-21 21:36:26 -0300

    add more itens at the list

--- app/src/main/java/com/youtubeclonecourse/Model/Videos.kt
+++ app/src/main/java/com/youtubeclonecourse/Model/Videos.kt
@@ -2,15 +2,16 @@ package com.youtubeclonecourse.Model
 
 import com.youtubeclonecourse.R
 
-data class Videos (
+data class Videos(
 
     val thumb: Int,
     val photo: Int,
     val timeVideo: String,
     val title: String,
-    val views: String)
+    val views: String
+)
 
-class VideosBuilder{
+class VideosBuilder {
     var thumb: Int = 0
     var photo: Int = 0
     var timeVideo: String = ""
@@ -18,12 +19,12 @@ class VideosBuilder{
     var views: String = ""
 
 
-    fun build(): Videos = Videos(thumb,photo,timeVideo,title,views)
+    fun build(): Videos = Videos(thumb, photo, timeVideo, title, views)
 }
 
 fun videos(block: VideosBuilder.() -> Unit): Videos = VideosBuilder().apply(block).build()
 
-fun addVideos():MutableList<Videos> = mutableListOf(
+fun addVideos(): MutableList<Videos> = mutableListOf(
 
     videos {
 
@@ -32,6 +33,22 @@ fun addVideos():MutableList<Videos> = mutableListOf(
         timeVideo = "25:00"
         title = "Minecraft: O Inicio de gameplay"
         views = "Minecraft 56 mil visializações - 8 meses"
+    },
+
+    videos {
+        thumb = R.drawable.viagens
+        photo = R.drawable.canal3
+        timeVideo = "12:00"
+        title = "Conheça as Ilhas Maldivas"
+        views = "ViagensBr 1 mi visualizações - 1 ano"
+    },
+
+    videos {
+        thumb = R.drawable.animes
+        photo = R.drawable.canal4
+        timeVideo = "30:00"
+        title = "Dragon Ball Super - Episódio 50"
+        views = "Animes 98 mil visualizações - 1 mês"
     }
 
 )
